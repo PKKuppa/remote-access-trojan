743e11f30f0f2dbe037f9d1904384e5285f61200
PKKuppa <pranay.kuppa@gmail.com>
2024-06-12 15:20:01 -0400

    Wrote skeleton for keylogger

---
+++ keylogger/KeyLogger.py
@@ -0,0 +1,14 @@
+
+import win32api
+
+
+class KeyLogger:
+
+    def __init__(self, outfile):
+        self.outfile = outfile  
+
+    def readKeypress(event):
+        pass
+
+    def writeKeypress(input):
+        pass
