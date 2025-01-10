64bcacd61950237b05d1ed838c04e168fe86c725
Pranay Kuppa <pkuppa@umd.edu>
2025-01-09 21:02:44 -0500

    Fixed Permission Denied errors ruining my life

--- file_enumeration/win_enum.ps1
+++ file_enumeration/win_enum.ps1
@@ -1,9 +1,14 @@
 #Searching for executables that can be run as admin
 function Enumerate-Files {
     #Need to get dlls as well
-    Get-Childitem -Recurse -Filter *.exe -Path C:\ | ForEach-Object{
-        Get-Acl -Path $_.FullName
+    #Need to filter by
+    $exec = @()
+    Get-Childitem -Recurse -Filter *.exe -Path C:\ -ErrorAction SilentlyContinue | ForEach-Object{
+
+       try{ $acl = Get-Acl -Path $_.FullName} catch{$acl = "Permission Denied"}
+        #Need to parse acl
     }
+    
     
 }
 
